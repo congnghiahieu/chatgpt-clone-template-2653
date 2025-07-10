96266c2b04462468815f74d80b9eee3ae6258b71
congnghiahieu <hieucn.work@gmail.com>
2025-07-10 22:39:31 +0700

    feat: openapi-ts config to generate client API code

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { componentTagger } from 'lovable-tagger';
 
 export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === 'development' && componentTagger()].filter(Boolean),
   server: {
     host: '0.0.0.0',
     port: 3000,
